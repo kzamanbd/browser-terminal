1fdba450d28c0173282107ee2020ea3f550a292a
KAMRUZZAMAN <kzamanbn@gmail.com>
2024-08-15 08:58:00 +0600

    refactor: src directory name

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -1,23 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    resolve: {
-        alias: {
-            '@': resolve(__dirname, './app')
-        }
-    },
-    build: {
-        rollupOptions: {
-            input: {
-                main: resolve(__dirname, 'app/main.tsx')
-            }
-        }
-    },
-    server: {
-        port: 3000
-    }
 });
