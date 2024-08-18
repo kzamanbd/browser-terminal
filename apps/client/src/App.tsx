dbdfde28c0d4e4f1f7d7000e2183afaa28c4e6d6
KAMRUZZAMAN <kzamanbn@gmail.com>
2024-08-18 20:41:34 +0600

    feat: added react router and layouts

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,7 +1,23 @@
+import Index from './pages/Index';
 import XTerminal from './pages/XTerminal';
 
-function App() {
-    return <XTerminal />;
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        Component: Index
+    },
+    {
+        path: '/ssh',
+        Component: XTerminal
+    }
+]);
+
+export default function App() {
+    return <RouterProvider router={router} fallbackElement={<p>Loading...</p>} />;
 }
 
-export default App;
+if (import.meta.hot) {
+    import.meta.hot.dispose(() => router.dispose());
+}
